34a2a91d493f22b95757e77c2d087f63a18cadb1
Kirk Klobe <kklobe@gmail.com>
2018-04-05 13:15:18 -0500

    add windowing functions

---
+++ window/window.go
@@ -0,0 +1,114 @@
+package window
+
+import (
+	"errors"
+	"math"
+)
+
+// Hann returns an n-sized Hann window
+func Hann(n int) ([]float64, error) {
+	if n < 1 {
+		return nil, errors.New("invalid window size")
+	}
+	w := make([]float64, n)
+	if n == 1 {
+		w[0] = 1
+	} else {
+		k := 2 * math.Pi / float64(n-1)
+
+		for i := range w {
+			w[i] = 0.5 * (1 - math.Cos(k*float64(i)))
+		}
+	}
+	return w, nil
+}
+
+// Hamming returns an n-sized Hamming window
+func Hamming(n int) ([]float64, error) {
+	if n < 1 {
+		return nil, errors.New("invalid window size")
+	}
+	w := make([]float64, n)
+	if n == 1 {
+		w[0] = 1
+	} else {
+		k := 2 * math.Pi / float64(n-1)
+
+		for i := range w {
+			w[i] = 0.54 -
+				0.46*math.Cos(k*float64(i))
+		}
+	}
+	return w, nil
+}
+
+// Blackman returns an n-sized Blackman window
+func Blackman(n int) ([]float64, error) {
+	if n < 1 {
+		return nil, errors.New("invalid window size")
+	}
+	w := make([]float64, n)
+	if n == 1 {
+		w[0] = 1
+	} else {
+		k := 2 * math.Pi / float64(n-1)
+
+		for i := range w {
+			w[i] = 0.42 -
+				0.50*math.Cos(k*float64(i)) +
+				0.08*math.Cos(2*k*float64(i))
+		}
+	}
+	return w, nil
+}
+
+// BlackmanHarris returns an n-sized Blackman-Harris window
+func BlackmanHarris(n int) ([]float64, error) {
+	if n < 1 {
+		return nil, errors.New("invalid window size")
+	}
+	w := make([]float64, n)
+	if n == 1 {
+		w[0] = 1
+	} else {
+		k := 2 * math.Pi / float64(n-1)
+
+		for i := range w {
+			w[i] = 0.35875 -
+				0.48829*math.Cos(k*float64(i)) +
+				0.14128*math.Cos(2*k*float64(i)) -
+				0.01168*math.Cos(3*k*float64(i))
+		}
+	}
+	return w, nil
+}
+
+// Bartlett returns an n-sized Bartlett window
+func Bartlett(n int) ([]float64, error) {
+	if n < 1 {
+		return nil, errors.New("invalid window size")
+	}
+	w := make([]float64, n)
+	if n == 1 {
+		w[0] = 1
+	} else {
+		n0 := float64(n - 1)
+		for i := range w {
+			i0 := float64(i)
+			w[i] = 2 / n0 * (n0/2 - math.Abs(i0-n0/2))
+		}
+	}
+	return w, nil
+}
+
+// Dirichlet returns an n-sized Dirichlet (rectangluar) window
+func Dirichlet(n int) ([]float64, error) {
+	if n < 1 {
+		return nil, errors.New("invalid window size")
+	}
+	w := make([]float64, n)
+	for i := range w {
+		w[i] = 1
+	}
+	return w, nil
+}
---
+++ window/window_test.go
@@ -0,0 +1,126 @@
+package window
+
+import (
+	"math"
+	"testing"
+)
+
+func TestHann(t *testing.T) {
+	n := 16
+	// SciPy: numpy.set_printoptions(precision=15); scipy.signal.hann(16)
+	v := []float64{
+		0., 0.0432272711787, 0.165434696820571,
+		0.345491502812526, 0.552264231633827, 0.75,
+		0.904508497187474, 0.989073800366903, 0.989073800366903,
+		0.904508497187474, 0.75, 0.552264231633827,
+		0.345491502812526, 0.165434696820571, 0.0432272711787,
+		0.,
+	}
+	w, _ := Hann(n)
+
+	sum := 0.0
+	for i := 0; i < n; i++ {
+		sum += (v[i] - w[i]) * (v[i] - w[i])
+	}
+
+	mse := math.Sqrt(sum / float64(n))
+	if mse > 1e-15 {
+		t.Error("window error, MSE:", mse)
+	}
+}
+
+func TestHamming(t *testing.T) {
+	n := 16
+	// SciPy: numpy.set_printoptions(precision=15); scipy.signal.hamming(16)
+	v := []float64{
+		0.08, 0.119769089484404, 0.232199921074925,
+		0.397852182587524, 0.588083093103121, 0.77,
+		0.912147817412476, 0.989947896337551, 0.989947896337551,
+		0.912147817412476, 0.77, 0.588083093103121,
+		0.397852182587524, 0.232199921074925, 0.119769089484404,
+		0.08,
+	}
+	w, _ := Hamming(n)
+	sum := 0.0
+	for i := 0; i < n; i++ {
+		sum += (v[i] - w[i]) * (v[i] - w[i])
+	}
+
+	mse := math.Sqrt(sum / float64(n))
+	if mse > 1e-15 {
+		t.Error("window error, MSE:", mse)
+	}
+}
+
+func TestBlackman(t *testing.T) {
+	n := 16
+	// SciPy: numpy.set_printoptions(precision=15); scipy.signal.blackman(16)
+	v := []float64{
+		-1.387778780781446e-17, 1.675771968740822e-02,
+		7.707241975915854e-02, 2.007701432625305e-01,
+		3.940124235751222e-01, 6.299999999999999e-01,
+		8.492298567374694e-01, 9.821574369783108e-01,
+		9.821574369783108e-01, 8.492298567374694e-01,
+		6.300000000000002e-01, 3.940124235751227e-01,
+		2.007701432625306e-01, 7.707241975915853e-02,
+		1.675771968740816e-02, -1.387778780781446e-17,
+	}
+	w, _ := Blackman(n)
+	sum := 0.0
+	for i := 0; i < n; i++ {
+		sum += (v[i] - w[i]) * (v[i] - w[i])
+	}
+
+	mse := math.Sqrt(sum / float64(n))
+	if mse > 1e-15 {
+		t.Error("window error, MSE:", mse)
+	}
+}
+
+func TestBlackmanHarris(t *testing.T) {
+	n := 16
+	// SciPy: numpy.set_printoptions(precision=15); scipy.signal.blackmanharris(16)
+	v := []float64{
+		6.000000000000102e-05, 3.600342059774550e-03,
+		2.670175342487845e-02, 1.030114893456638e-01,
+		2.679881918029911e-01, 5.205749999999999e-01,
+		7.938335106543363e-01, 9.748847127123560e-01,
+		9.748847127123561e-01, 7.938335106543363e-01,
+		5.205750000000002e-01, 2.679881918029914e-01,
+		1.030114893456638e-01, 2.670175342487850e-02,
+		3.600342059774529e-03, 6.000000000000102e-05,
+	}
+	w, _ := BlackmanHarris(n)
+	sum := 0.0
+	for i := 0; i < n; i++ {
+		sum += (v[i] - w[i]) * (v[i] - w[i])
+	}
+
+	mse := math.Sqrt(sum / float64(n))
+	if mse > 1e-15 {
+		t.Error("window error, MSE:", mse)
+	}
+}
+
+func TestBartlett(t *testing.T) {
+	n := 16
+	// SciPy: numpy.set_printoptions(precision=15); scipy.signal.bartlett(16)
+	v := []float64{
+		0., 0.133333333333333, 0.266666666666667,
+		0.4, 0.533333333333333, 0.666666666666667,
+		0.8, 0.933333333333333, 0.933333333333333,
+		0.8, 0.666666666666667, 0.533333333333333,
+		0.4, 0.266666666666667, 0.133333333333333,
+		0.,
+	}
+	w, _ := Bartlett(n)
+	sum := 0.0
+	for i := 0; i < n; i++ {
+		sum += (v[i] - w[i]) * (v[i] - w[i])
+	}
+
+	mse := math.Sqrt(sum / float64(n))
+	if mse > 1e-15 {
+		t.Error("window error, MSE:", mse)
+	}
+}
